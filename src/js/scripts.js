41f3a49c474e565677d5ff3ad3726bad67344c26
superigrooper <superigrooper@gmail.com>
2021-02-04 23:01:05 +0300

    slider

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,17 +1,42 @@
-window.addEventListener('DOMContentLoaded', () => {
-    const nav = document.querySelector('.promo__nav');
-    const menuItem = document.querySelectorAll('.promo__nav li');
-    const hamburger = document.querySelector('.promo__hamburger');
-
-    hamburger.addEventListener('click', () => {
-        hamburger.classList.toggle('promo__hamburger_active');
-        nav.classList.toggle('promo__nav_active');
+$(document).ready(function () {
+    $('.carousel__inner').slick({
+        speed: 1200,
+        prevArrow: '<button class="buttons-arrow__item left"><img src="img/icons/left.svg" alt=""></button>',
+        nextArrow: '<button class="buttons-arrow__item right"><img src="img/icons/right.svg" alt=""></button>',
+        settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3
+        },
+        responsive: [
+            {
+                breakpoint: 992,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    infinite: true,
+                    dots: false,
+                    arrows: false,
+                    autoplay: true
+                }
+            }
+        ]
     });
+});
 
-    menuItem.forEach(item => {
-        item.addEventListener('click', () => {
+    window.addEventListener('DOMContentLoaded', () => {
+        const nav = document.querySelector('.promo__nav');
+        const menuItem = document.querySelectorAll('.promo__nav li');
+        const hamburger = document.querySelector('.promo__hamburger');
+
+        hamburger.addEventListener('click', () => {
             hamburger.classList.toggle('promo__hamburger_active');
             nav.classList.toggle('promo__nav_active');
         });
+
+        menuItem.forEach(item => {
+            item.addEventListener('click', () => {
+                hamburger.classList.toggle('promo__hamburger_active');
+                nav.classList.toggle('promo__nav_active');
+            });
+        });
     });
-});
